1979153a58c811632faa1a22667ccf7fb203920b
dsnz <esarmas@gmail.com>
2016-05-25 18:20:22 +0300

    Added json_printer.empty_string_as_null property (by Vladislafff/develop)

--- src/pljson_printer.sql
+++ src/pljson_printer.sql
@@ -22,6 +22,7 @@ create or replace package pljson_printer as
   --newline_char varchar2(2) := chr(10); -- Mac style
   --newline_char varchar2(2) := chr(13); -- Linux style
   ascii_output boolean    not null := true;
+  empty_string_as_null boolean not null := false;
   escape_solidus boolean  not null := false;
   
   function pretty_print(obj pljson, spaces boolean default true, line_length number default 0) return varchar2;
@@ -192,8 +193,11 @@ create or replace package body pljson_printer as
     v_str varchar(5000 char);
     amount number := 5000; /* chunk size for use in escapeString; maximum escaped unicode string size for chunk may be 6 one-byte chars * 5000 chunk size in multi-byte chars = 30000 1-byte chars (maximum value is 32767 1-byte chars) */
   begin
-    add_to_clob(buf, buf_str, case when elem.num = 1 then '"' else '/**/' end);
-    if(elem.extended_str is not null) then --clob implementation
+    if empty_string_as_null and elem.extended_str is null and elem.str is null then
+      add_to_clob(buf, buf_str, 'null');
+    else
+      add_to_clob(buf, buf_str, case when elem.num = 1 then '"' else '/**/' end);
+      if(elem.extended_str is not null) then --clob implementation
         while(offset <= dbms_lob.getlength(elem.extended_str)) loop
           dbms_lob.read(elem.extended_str, amount, offset, v_str);
           if(elem.num = 1) then
@@ -203,18 +207,19 @@ create or replace package body pljson_printer as
           end if;
           offset := offset + amount;
         end loop;
-    else
+      else
         if(elem.num = 1) then
-            while(offset <= length(elem.str)) loop
-                v_str:=substr(elem.str, offset, amount);
-                add_to_clob(buf, buf_str, escapeString(v_str));
-                offset := offset + amount;
-            end loop;
+          while(offset <= length(elem.str)) loop
+            v_str:=substr(elem.str, offset, amount);
+            add_to_clob(buf, buf_str, escapeString(v_str));
+            offset := offset + amount;
+          end loop;
         else
-            add_to_clob(buf, buf_str, elem.str);
+          add_to_clob(buf, buf_str, elem.str);
         end if;
+      end if;
+      add_to_clob(buf, buf_str, case when elem.num = 1 then '"' else '/**/' end);
     end if;
-    add_to_clob(buf, buf_str, case when elem.num = 1 then '"' else '/**/' end || newline_char);
   end;
   
   procedure ppEA(input pljson_list, indent number, buf in out nocopy clob, spaces boolean, buf_str in out nocopy varchar2) as
@@ -377,7 +382,7 @@ create or replace package body pljson_printer as
   procedure add_buf (buf in out nocopy varchar2, str in varchar2) as
   begin
     if(lengthb(str)>32767-lengthb(buf)) then
-        raise_application_error(-20001,'Length of result JSON more than 32767 bytes. Use to_clob() procedures');
+      raise_application_error(-20001,'Length of result JSON more than 32767 bytes. Use to_clob() procedures');
     end if;
     buf := buf || str;
   end;
@@ -388,8 +393,11 @@ create or replace package body pljson_printer as
     v_str varchar(5000 char);
     amount number := 5000; /* chunk size for use in escapeString; maximum escaped unicode string size for chunk may be 6 one-byte chars * 5000 chunk size in multi-byte chars = 30000 1-byte chars (maximum value is 32767 1-byte chars) */
   begin
-    add_buf(buf, case when elem.num = 1 then '"' else '/**/' end);
-    if(elem.extended_str is not null) then --clob implementation
+    if empty_string_as_null and elem.extended_str is null and elem.str is null then
+      add_buf(buf, 'null');
+    else
+      add_buf(buf, case when elem.num = 1 then '"' else '/**/' end);
+      if(elem.extended_str is not null) then --clob implementation
         while(offset <= dbms_lob.getlength(elem.extended_str)) loop
           dbms_lob.read(elem.extended_str, amount, offset, v_str);
           if(elem.num = 1) then
@@ -399,18 +407,19 @@ create or replace package body pljson_printer as
           end if;
           offset := offset + amount;
         end loop;
-    else
+      else
         if(elem.num = 1) then
-            while(offset <= length(elem.str)) loop
-                v_str:=substr(elem.str, offset, amount);
-                add_buf(buf, escapeString(v_str));
-                offset := offset + amount;
-            end loop;
+          while(offset <= length(elem.str)) loop
+            v_str:=substr(elem.str, offset, amount);
+            add_buf(buf, escapeString(v_str));
+            offset := offset + amount;
+          end loop;
         else
-            add_buf(buf, elem.str);
+          add_buf(buf, elem.str);
         end if;
+      end if;
+      add_buf(buf, case when elem.num = 1 then '"' else '/**/' end);
     end if;
-    add_buf(buf, case when elem.num = 1 then '"' else '/**/' end || newline_char);
   end;
   
   procedure ppObj(obj pljson, indent number, buf in out nocopy varchar2, spaces boolean);
