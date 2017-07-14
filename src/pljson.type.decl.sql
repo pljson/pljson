db04f4ecc4748853085ebf35b441c888a7d268f1
James Sumners <james.sumners@gmail.com>
2017-07-14 13:49:41 -0400

    Round out pljson type documentation

--- src/pljson.type.decl.sql
+++ src/pljson.type.decl.sql
@@ -35,6 +35,17 @@ create or replace type pljson as object (
    * <p>The primary method exported by this package is the <code>pljson</code>
    * method.</p>
    *
+   * <em>Example:</em>
+   * <pre>
+   * declare
+   *   myjson pljson := pljson('{ "foo": "foo", "bar": [0, 1, 2], "baz": { "foobar": "foobar" } }');
+   * begin
+   *   pljson.get('foo').print(); // => dbms_output.put_line('foo')
+   *   pljson.get('bar[1]').print(); // => dbms_output.put_line('0')
+   *   pljson.get('baz.foobar').print(); // => dbms_output.put_line('foobar')
+   * end;
+   * </pre>
+   *
    * @headcom
    */
 
@@ -48,6 +59,8 @@ create or replace type pljson as object (
 
   /**
    * <p>Primary constructor that creates an empty object.</p>
+   *
+   * <p>Internally, a <code>pljson</code> "object" is an array of values.</p>
    *
    * <pre>
    *   decleare
@@ -73,43 +86,223 @@ create or replace type pljson as object (
    *   end;
    * </pre>
    *
+   * @param str The JSON to parse into a <code>pljson</code> object.
    * @return A <code>pljson</code> instance.
    */
   constructor function pljson(str varchar2) return self as result,
+
+  /**
+   * <p>Construct a <code>pljson</code> instance from a given CLOB of JSON.</p>
+   *
+   * @param str The CLOB to parse into a <code>pljson</code> object.
+   * @return A <code>pljson</code> instance.
+   */
   constructor function pljson(str in clob) return self as result,
+
+  /**
+   * <p>Create a new <code>pljson</code> object from a current <code>pljson_value</code>.
+   *
+   * <pre>
+   *   declare
+   *    myjson pljson := pljson('{"foo": {"bar": "baz"}}');
+   *    newjson pljson;
+   *   begin
+   *    newjson := pljson(myjson.get('foo').to_json_value())
+   *   end;
+   * </pre>
+   *
+   * @param cast The value to create a new object from.
+   * @return A <code>pljson</code> instance.
+   */
   constructor function pljson(cast pljson_value) return self as result,
+
+  /**
+   * <p>Create a new <code>pljson</code> object from a current <code>pljson_list</code>.
+   *
+   * @param l The array to create a new object from.
+   * @return A <code>pljson</code> instance.
+   */
   constructor function pljson(l in out nocopy pljson_list) return self as result,
 
   /* Member setter methods */
+  /**
+   * <p>Remove a key and value from an object.</p>
+   *
+   * <pre>
+   *   declare
+   *     myjson pljson := pljson('{"foo": "foo", "bar": "bar"}')
+   *   begin
+   *     myjson.remove('bar'); // => '{"foo": "foo"}'
+   *   end;
+   * </pre>
+   *
+   * @param pair_name The key name to remove.
+   */
   member procedure remove(pair_name varchar2),
+
+  /**
+   * <p>Add a <code>pljson</code> instance into the current instance under a
+   * given key name.</p>
+   *
+   * @param pair_name Name of the key to add/update.
+   * @param pair_value The value to associate with the key.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value pljson_value, position pls_integer default null),
+
+  /**
+   * <p>Add a <code>varchar2</code> instance into the current instance under a
+   * given key name.</p>
+   *
+   * @param pair_name Name of the key to add/update.
+   * @param pair_value The value to associate with the key.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value varchar2, position pls_integer default null),
+
+  /**
+   * <p>Add a <code>number</code> instance into the current instance under a
+   * given key name.</p>
+   *
+   * @param pair_name Name of the key to add/update.
+   * @param pair_value The value to associate with the key.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value number, position pls_integer default null),
+
   /* E.I.Sarmas (github.com/dsnz)   2016-12-01   support for binary_double numbers */
+  /**
+   * <p>Add a <code>binary_double</code> instance into the current instance under a
+   * given key name.</p>
+   *
+   * @param pair_name Name of the key to add/update.
+   * @param pair_value The value to associate with the key.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value binary_double, position pls_integer default null),
+
+  /**
+   * <p>Add a <code>boolean</code> instance into the current instance under a
+   * given key name.</p>
+   *
+   * @param pair_name Name of the key to add/update.
+   * @param pair_value The value to associate with the key.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value boolean, position pls_integer default null),
+
   member procedure check_duplicate(self in out nocopy pljson, v_set boolean),
   member procedure remove_duplicates(self in out nocopy pljson),
 
   /* deprecated putter use pljson_value */
+  /**
+   * @deprecated Use <code>pljson_value</code> method instead.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value pljson, position pls_integer default null),
+  /**
+   * @deprecated Use <code>pljson_value</code> method instead.
+   */
   member procedure put(self in out nocopy pljson, pair_name varchar2, pair_value pljson_list, position pls_integer default null),
 
   /* Member getter methods */
+  /**
+   * <p>Return the number values in the object. Essentially, the number of keys
+   * in the object.</p>
+   *
+   * @return The number of values in the object.
+   */
   member function count return number,
+
+  /**
+   * <p>Retrieve the value of a given key.</p>
+   *
+   * @param pair_name The name of the value to retrieve.
+   * @return An instance of <code>pljson_value</code>, or <code>null</code>
+   * if it could not be found.
+   */
   member function get(pair_name varchar2) return pljson_value,
+
+  /**
+   * <p>Retrieve a value based on its position in the internal storage array.
+   * It is recommended you use name based retrieval.</p>
+   *
+   * @param position Index of the value in the internal storage array.
+   * @return An instance of <code>pljson_value</code>, or <code>null</code>
+   * if it could not be found.
+   */
   member function get(position pls_integer) return pljson_value,
+
+  /**
+   * <p>Determine the position of a given value within the internal storage
+   * array.</p>
+   *
+   * @param pair_name The name of the value to retrieve the index for.
+   * @return An index number, or <code>-1</code> if it could not be found.
+   */
   member function index_of(pair_name varchar2) return number,
+
+  /**
+   * <p>Determine if a given value exists within the object.</p>
+   *
+   * @param pair_name The name of the value to check for.
+   * @return <code>true</code> if the value exists, <code>false</code> otherwise.
+   */
   member function exist(pair_name varchar2) return boolean,
 
   /* Output methods */
+  /**
+   * <p>Serialize the object to a JSON representation string.</p>
+   *
+   * @param spaces Enable pretty printing by formatting with spaces. Default: <code>true</code>.
+   * @param chars_per_line Wrap output to a specific number of characters per line. Default: <code>0<code> (infinite).
+   * @return A <code>varchar2</code> string.
+   */
   member function to_char(spaces boolean default true, chars_per_line number default 0) return varchar2,
+
+  /**
+   * <p>Serialize the object to a JSON representation and store it in a CLOB.</p>
+   *
+   * @param buf The CLOB in which to store the results.
+   * @param spaces Enable pretty printing by formatting with spaces. Default: <code>false</code>.
+   * @param chars_per_line Wrap output to a specific number of characters per line. Default: <code>0<code> (infinite).
+   * @param erase_clob Whether or not to wipe the storage CLOB prior to serialization. Default: <code>true</code>.
+   * @return A <code>varchar2</code> string.
+   */
   member procedure to_clob(self in pljson, buf in out nocopy clob, spaces boolean default false, chars_per_line number default 0, erase_clob boolean default true),
+
+  /**
+   * <p>Print a JSON representation of the object via <code>DBMS_OUTPUT</code>.</p>
+   *
+   * @param spaces Enable pretty printing by formatting with spaces. Default: <code>true</code>.
+   * @param chars_per_line Wrap output to a specific number of characters per line. Default: <code>8192<code> (<code>32512</code> is maximum).
+   * @param jsonp Name of a function for wrapping the output as JSONP. Default: <code>null</code>.
+   * @return A <code>varchar2</code> string.
+   */
   member procedure print(self in pljson, spaces boolean default true, chars_per_line number default 8192, jsonp varchar2 default null), --32512 is maximum
+
+  /**
+   * <p>Print a JSON representation of the object via <code>HTP.PRN</code>.</p>
+   *
+   * @param spaces Enable pretty printing by formatting with spaces. Default: <code>true</code>.
+   * @param chars_per_line Wrap output to a specific number of characters per line. Default: <code>0<code> (infinite).
+   * @param jsonp Name of a function for wrapping the output as JSONP. Default: <code>null</code>.
+   * @return A <code>varchar2</code> string.
+   */
   member procedure htp(self in pljson, spaces boolean default false, chars_per_line number default 0, jsonp varchar2 default null),
 
+  /**
+   * <p>Convert the object to a <code>pljson_value</code> for use in other methods
+   * of the PL/JSON API.</p>
+   *
+   * @returns An instance of <code>pljson_value</code>.
+   */
   member function to_json_value return pljson_value,
+
   /* json path */
+  /**
+   * <p>Retrieve a value from the internal storage array based on a path string
+   * and a starting index.</p>
+   *
+   * @param json_path A string path, e.g. <code>'foo.bar[1]'</code>.
+   * @param base The index in the internal storage array to start from.
+   * This should only be necessary under special circumstances. Default: <code>1</code>.
+   * @return An instance of <code>pljson_value</code>.
+   */
   member function path(json_path varchar2, base number default 1) return pljson_value,
 
   /* json path_put */
@@ -126,7 +319,28 @@ create or replace type pljson as object (
   member procedure path_remove(self in out nocopy pljson, json_path varchar2, base number default 1),
 
   /* map functions */
+  /**
+   * <p>Retrieve all of the values within the object as a <code>pljson_list</code>.</p>
+   *
+   * <pre>
+   * myjson := pljson('{"foo": "bar"}');
+   * myjson.get_values(); // ['bar']
+   * </pre>
+   *
+   * @return An instance of <code>pljson_list</code>.
+   */
   member function get_values return pljson_list,
+
+  /**
+   * <p>Retrieve all of the keys within the object as a <code>pljson_list</code>.</p>
+   *
+   * <pre>
+   * myjson := pljson('{"foo": "bar"}');
+   * myjson.get_keys(); // ['foo']
+   * </pre>
+   *
+   * @return An instance of <code>pljson_list</code>.
+   */
   member function get_keys return pljson_list
 
 ) not final;
