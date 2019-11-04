91df42a2d9364e26560cf62764b1adc47945eca7
Kaleb Hall <wkhall@Kalebs-MacBook-Pro.local>
2019-11-04 14:51:58 -0600

    new example of retrieve json results as clob

---
+++ examples/ex20.sql
@@ -0,0 +1,25 @@
+/* retrieving sql results as json clob */
+set serveroutput on;
+create or replace function ufx_plson_get_json_from_sql (i_sql in varchar2)
+return clob
+as
+  -- Local variables here
+  tstjson_list pljson_list;
+  l_Result_json_clob clob;
+begin
+  -- Test statements here
+  tstjson_list := pljson_list();
+  dbms_lob.createtemporary(l_Result_json_clob, true);
+
+  tstjson_list := pljson_util_pkg.sql_to_json(i_sql);
+  tstjson_list.to_clob(l_Result_json_clob);
+  
+  return l_Result_json_clob;
+exception
+  when others then raise;
+end;
+/
+/* example */
+select ufx_plson_get_json_from_sql (i_sql =>
+          q'[ select sysdate, systimestamp, user from dual]') sql_result_as_json
+  from dual;
