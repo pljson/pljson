b28a8524352582376786d15a3e4bcf872a3b0ea8
E.I.Sarmas <dsnz@users.noreply.github.com>
2020-12-18 22:20:22 +0200

    backport 1/2 from v3.5.0 -------------------- #217   fix for issue #213 (sql_to_json() with special characters) #225   Return early on 0 rows, and remove the unused g_json_null_object constant #225   Rearrange code for readability #225   Workaround for Oracle bug with XMLTYPE [fixes issue #222; Reverts the fix for issue #213] #238   fix issue #237 (number format of pljson_util_pkg.sql_to_json -------------------- #226   Avoid overlapping printing of data -------------------- #228   Do not advertise commercial software

--- src/addons/pljson_util_pkg.package.sql
+++ src/addons/pljson_util_pkg.package.sql
@@ -51,9 +51,6 @@ create or replace package body pljson_util_pkg as
   */
 
 
-  g_json_null_object             constant varchar2(20) := '{ }';
-
-
 function get_xml_to_json_stylesheet return varchar2 as
 begin
 
@@ -208,7 +205,14 @@ not(starts-with(.,'0.' ))) or
                       (starts-with(.,'-0' ) and . != '-0' and
 not(starts-with(.,'-0.' )))
                       )]">
-    <xsl:value-of select="."/>
+    <xsl:choose>
+      <xsl:when test="starts-with(., '.')">
+        <xsl:value-of select="concat('0', .)"/>
+      </xsl:when>
+      <xsl:otherwise>
+        <xsl:value-of select="."/>
+      </xsl:otherwise>
+    </xsl:choose>
   </xsl:template>
   
   <!-- boolean, case-insensitive -->
@@ -268,7 +272,7 @@ as
   l_ctx         dbms_xmlgen.ctxhandle;
   l_num_rows    pls_integer;
   l_xml         xmltype;
-  l_json        xmltype;
+  l_xsl         xmltype         := xmltype(get_xml_to_json_stylesheet);
   l_returnvalue clob;
 begin
 
@@ -286,56 +290,53 @@ begin
   */
 
   l_ctx := dbms_xmlgen.newcontext (p_ref_cursor);
-
+  
   dbms_xmlgen.setnullhandling (l_ctx, dbms_xmlgen.empty_tag);
-
+  
   -- for pagination
-
+  
   if p_max_rows is not null then
     dbms_xmlgen.setmaxrows (l_ctx, p_max_rows);
   end if;
-
+  
   if p_skip_rows is not null then
     dbms_xmlgen.setskiprows (l_ctx, p_skip_rows);
   end if;
-
+  
   -- get the XML content
   l_xml := dbms_xmlgen.getxmltype (l_ctx, dbms_xmlgen.none);
-
+  
   l_num_rows := dbms_xmlgen.getnumrowsprocessed (l_ctx);
-
+  
   dbms_xmlgen.closecontext (l_ctx);
-
+  
   close p_ref_cursor;
-  --dbms_output.put_line(l_xml.getstringval);
-  if l_num_rows > 0 then
-    -- perform the XSL transformation
-    l_json := l_xml.transform (xmltype(get_xml_to_json_stylesheet));
-    --dbms_output.put_line(l_json.getstringval);
-    l_returnvalue := l_json.getclobval();
-  else
-    l_returnvalue := g_json_null_object;
-  end if;
 
-  l_returnvalue := dbms_xmlgen.convert (l_returnvalue, dbms_xmlgen.entity_decode);
-  --dbms_output.put_line(l_returnvalue);
   if(l_num_rows = 0) then
     return pljson_list();
-  else
-    if(l_num_rows = 1) then
-      declare ret pljson_list := pljson_list();
-      begin
-        ret.append(
-          pljson(
-            pljson(l_returnvalue).get('ROWSET')
-          ).get('ROW')
-        );
-        return ret;
-      end;
-    else
-      return pljson_list(pljson(l_returnvalue).get('ROWSET'));
-    end if;
   end if;
+
+  --dbms_output.put_line(l_xml.getstringval);
+  -- perform the XSL transformation
+  SELECT  l_xml.transform(l_xsl).getclobval()
+  INTO    l_returnvalue
+  FROM    DUAL;
+
+  --dbms_output.put_line(l_returnvalue);
+
+  if(l_num_rows > 1) then
+    return pljson_list(pljson(l_returnvalue).get('ROWSET'));
+  end if;
+
+  declare ret pljson_list := pljson_list();
+  begin
+    ret.append(
+      pljson(
+        pljson(l_returnvalue).get('ROWSET')
+      ).get('ROW')
+    );
+    return ret;
+  end;
 
 exception
   when scanner_exception then
@@ -347,7 +348,6 @@ exception
     dbms_output.put_line(l_returnvalue);
     raise;
   when others then raise;
-
 end ref_cursor_to_json;
 
 function sql_to_json (p_sql in varchar2,
